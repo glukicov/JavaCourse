a5b0e0f74001d54bb3cbf3e661b02d44725f907d
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2014-10-06 19:30:42 +0100

    Module 1 - completed!!!

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -28,41 +28,55 @@ public class AlgorithmControl {
 		double inc_val;
 		double finish=14.9;
 		double step=0.5;
-		for(inc_val=2.4; inc_val<=finish; inc_val=inc_val+0.5){
+		for(inc_val=2.4; inc_val<=finish; inc_val=inc_val+step){
 			System.out.println("inc_val= "+inc_val);
 		}
 
 	}
-	
-	public int timer(){
-		int y=1;
-		long timeNow=System.currentTimeMillis();
-		return y;
+
+	public static int timer(long maxTime, int loopSteps){  
+		int counter=0;
+		int remainder;
+		long finish_time=System.currentTimeMillis()+maxTime*1000;  //Max time in seconds * 1000 = XXXX ms
+
+		while (System.currentTimeMillis()<=finish_time){  // The logical condition is to run for 4000 ms
+
+			counter++;										// Every time a while loop is executed counter is incremented by 1
+			remainder=counter % loopSteps;						
+			if (remainder == 0) {							// When the remainder is 0 - it is the 100th loop!
+				System.out.println("Number of loops run so far= "+counter);  // Prints after every 100 loops
+			}
+		}
+		System.out.println(""); // Empty print out statement to improve to readability on console output
+		System.out.println("The toal Number of loops run= "+counter);
+		System.out.println("");
+		System.out.println("For big time period of while loop (e.g. maxTime= 4000 ms) and loopSteps=100 Number of loops run so far = Total number of loops (multiple of 100)...(interesting)");
+		System.out.println("However, for small time period (e.g. 2 ms) it as expected, NOT necessarily equal. This is due to the println statement embedded into the while-if loop..");
+		System.out.println("When we change the loopSteps=50000, the println statement appears 500 less often on the screen and slow down the while loop execution less significantly,");
+		System.out.println("as a result, the total number of loops executed increased by a factor of ~20 (e.g. from 16702600 to 303290839, respectively)");
+		return counter;
+
 	}
 
 
 	public static void main(String[] args) {
 
-		//loop(); //Calling on the loop method
+		loop(); //Calling on the loop method
+
 		System.out.println(""); // Empty print out statement to improve to readability on console output
-		//decrement();  //Calling on the decrement method
+
+		decrement();  //Calling on the decrement method
+
 		System.out.println("");
-		//increment();
-		
-		long time1=System.currentTimeMillis();
-		System.out.println("Time now ="+time1/1000);
-		try {
-		    Thread.sleep(5000);                 //1000 milliseconds is one second.
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		long time2=System.currentTimeMillis();
-		System.out.println("Time now ="+time2/1000);
-		System.out.println("Time now ="+(time2-time1)/1000);
 
+		increment(); //Calling on the increment method
 
+		System.out.println("");
+		// timer();  When amount of time was hard-coded no method arguments were necessary
 
+		// timer(4, 100); (e.g The total Number of loops run= 16702600)
 
+		timer(4, 50000); // (e.g. The total Number of loops run= 303290839) 
 	}
 }
 
