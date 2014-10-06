32e9c9581031730505bf4c5eb04c742d077f0860
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2014-10-06 17:45:00 +0100

    wooo2

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -5,6 +5,8 @@ package module1;
 
 public class AlgorithmControl {
 
+	public AlgorithmControl(){}						// Creating an empty constructor 
+
 	public static void loop() { //Creating a void method loop() as defined below:
 		int i;
 		int f=20;
@@ -14,10 +16,10 @@ public class AlgorithmControl {
 	}
 
 	public static void decrement(){ //Creating a void method decrement() as defined below:
-		int x=-5;
-		while (x<11){  // Creating a while loop which prints value of x until x reaches 11
-			System.out.println("x= "+x); // For last print out x is still 10
-			x++;  
+		int x=5;
+		while (x>-11){  // Creating a while loop which prints value of x until x reaches -11
+			System.out.println("x= "+x); // For last print out x is still -10
+			x--;  
 		}			
 	}
 
@@ -31,13 +33,34 @@ public class AlgorithmControl {
 		}
 
 	}
+	
+	public int timer(){
+		int y=1;
+		long timeNow=System.currentTimeMillis();
+		return y;
+	}
 
 
 	public static void main(String[] args) {
 
-		// loop(); //Calling on the loop method
-		//	decrement();  //Calling on the decrement method
-		//	increment();
+		//loop(); //Calling on the loop method
+		System.out.println(""); // Empty print out statement to improve to readability on console output
+		//decrement();  //Calling on the decrement method
+		System.out.println("");
+		//increment();
+		
+		long time1=System.currentTimeMillis();
+		System.out.println("Time now ="+time1/1000);
+		try {
+		    Thread.sleep(5000);                 //1000 milliseconds is one second.
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		long time2=System.currentTimeMillis();
+		System.out.println("Time now ="+time2/1000);
+		System.out.println("Time now ="+(time2-time1)/1000);
+
+
 
 
 	}
--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -1,10 +1,8 @@
-/* This is Function/Methods assignment 
- * 
- * This script: 1) Calculates the magnitude of a vector with components x, y, z (as defined in the script)
- * 				2) Calculates the dotProduct of two vectors with components a1, a2, a3 and b1, b2, b3
- *				3) Calculates the angle of two vectors with components a1, a2, a3 and b1, b2, b3
- * */
-
+/* This is Function/Methods assignment. This script: 1) Calculates the magnitude of a vector with components x, y, z (as defined in the script)
+2) Calculates the dotProduct of two vectors with components a1, a2, a3 and b1, b2, b3
+3) Calculates the angle between two vectors with components a1, a2, a3 and b1, b2, b3
+4) Investigates what happends when angle between a vector and 0 vector are calculated
+ */
 package module1;
 
 public class VectorMethods {
@@ -15,20 +13,18 @@ public class VectorMethods {
 		return Math.sqrt(x*x+y*y+z*z);		// Defining the return of the function magnitude in terms of x, y an z 								
 	}
 
-	public double dotProduct (double a1, double a2, double a3, double b1, double b2, double b3){
-		return a1*b1+a2*b2+a3*b3;
-
-		public double angle ((double a1, double a2, double a3, double b1, double b2, double b3){
-
-		}
+	public double dotProduct (double i, double j, double k, double l, double m, double n){
+		return i*l+j*m+k*n;
 	}
+	public double angle (double i, double j, double k, double l, double m, double n){			//Creating angle function which uses the outputs of functions 
+																									//dotProduct and magnitude
+		return Math.toDegrees(Math.acos((dotProduct(i,j,k,l,m,n)/(magnitude(i,j,k)*magnitude(l,m,n)))));
+	}
+
 
 	public static void main(String[] args) {
 		VectorMethods vm = new VectorMethods(); // Creating vm object of VectorMethods class 
-		double x=4; 
-		double y=7; 
-		double z=3;
-
+	
 		double a1=6; 
 		double a2=5; 
 		double a3=1;
@@ -40,14 +36,35 @@ public class VectorMethods {
 		double scalarProduct=vm.dotProduct(a1,a2,a3,b1,b2,b3);
 		System.out.println("Scalar product of two vectors= "+scalarProduct+ " (With components: a1= "+a1+ " a2= "+a2+ " a3= "+a3+ " and " 
 				+ "b1= " +b1+ " b2= "+b2+ " b3= "+b3+ ")");
+		System.out.println(""); // Empty print out statement to improve to readability on console output
 
-		double mag = vm.magnitude(x,y,z); // Creating a double variable magnitude which calls the function "magnitude" and returns the magnitude
-		// of a vector with components as variables x, y and z
-		System.out.println("Vector Magnitude= "+mag+ " (With components: x= "+x+ " y= "+y+ " and z= "+z+")");
-
-		double theta=vm.angle(a1,a2,a3,b1,b2,b3);
-		System.out.println("The angle between two vectors= "+theta+ " (With components: x= "+x+ " y= "+y+ " and z= "+z+")");
-
-	}
+		double mag = vm.magnitude(a1,a2,a3); // Creating a double variable magnitude which calls the function "magnitude" and returns the magnitude
+										// of a vector with components as variables a1, a2 and a3
+		System.out.println("Vector Magnitude= "+mag+ " (With components: a1= "+a1+ " a2= "+a2+ " a3= "+a3+ " )");
+		System.out.println("");
+		double theta=vm.angle(a1,a2,a3,b1,b2,b3);  //Passing on the values a1, a2...as defined above to the angle function
+		System.out.println("The angle between two vectors= "+theta+ " (degrees) (With components: a1= "+a1+ " a2= "+a2+ " a3= "+a3+ " and " 
+				+ "b1= " +b1+ " b2= "+b2+ " b3= "+b3+ ")");
+		System.out.println("The result is as expected: a number in degrees which indicates the angle between the two vectors");
+		System.out.println("");
+		double c1=0;
+		double c2=0;
+		double c3=0;		
+		
+		double theta2=vm.angle(a1,a2,a3,c1,c2,c3);  //Now calcualting the scalar product with 0 vector..
+		System.out.println("The angle between two vectors= "+theta2+ " (With components: a1= "+a1+ " a2= "+a2+ " a3= "+a3+ " and " 
+				+ "c1= " +c1+ " c2= "+c2+ " c3= "+c3+ ")");
+		System.out.println("The result is NOT as expected: scalar prodcut with zero vector gives NaN (Not a number) output, because of the  divison by 0.0 which is arithmetically undefined (!):");
+		double mag2 = vm.magnitude(c1,c2,c3); // Creating a double variable magnitude which calls the function "magnitude" and returns the magnitude
+		System.out.println("Magnitude of 'zero' vector ="+mag2);
+		double scalarProduct2=vm.dotProduct(a1,a2,a3,c1,c2,c3);
+		System.out.println("Dot product with 'zero' vector ="+scalarProduct2);
+		System.out.println("And the input of the angle fucntions takes the following undefined input 0.0/0.0 (!)");
+		double theta3=Math.toDegrees(Math.acos(0.0/0.0));
+		System.out.println("The output= "+theta3);
+		System.out.println("However, IF the INTEGER was used for components of vector (int), then the devison woud look like 0/0 - whcih is NOT allowed (different than NaN) (!)");
+		// double theta4=Math.toDegrees(Math.acos(0/0)); The integer input, however triggers the explicit division by zero error (!)
+		
+		}
 
 }
