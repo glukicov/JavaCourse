8f21ffd11c801f038e0aff0b5879117b2862718d
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2014-10-06 15:37:52 +0100

    WOOOOOOOO

--- PHAS3459/src/module1/DataTypes.java
+++ PHAS3459/src/module1/DataTypes.java
@@ -5,37 +5,37 @@ package module1;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		
+
 		// Declaring and printing out variables: 
-		
+
 		double doubVar=10.0; // Declaring doubVar as double as assigning it value of 10.0
 		System.out.println("doubVar= "+doubVar);
 
 		float floatVar=10; 
 		System.out.println("floatVar= "+floatVar);
-		
+
 		int intVar=10; 
 		System.out.println("intVar= "+intVar);
-		
+
 		long longVar=10; 
 		System.out.println("longVar= "+longVar);
-		
+
 		byte byteVar=10; 
 		System.out.println("byteVar= "+byteVar);
-		
-		
+
+
 		// Multiplying and printing out variables:
-		
+
 		System.out.println("doubVar*doubVar= "+doubVar*doubVar);
 		System.out.println("floatVar*floatVar= "+floatVar*floatVar);
 		System.out.println("intVar*intVar= "+intVar*intVar);
 		System.out.println("longVar*longVar= "+longVar*longVar);
 		System.out.println("byteVar*longVar= "+byteVar*longVar);
-		
-	
+
+
 		// Investigating mixing types in 3 Examples:
 		//Example 1 character:
-		
+
 		char charVar='a';
 		System.out.println("charVar= " +charVar); //Prints out charVar single character variable
 		charVar='a'+10;
@@ -45,32 +45,32 @@ public class DataTypes {
 		charVar='a'-4;
 		System.out.println("charVar= " +charVar);
 		System.out.println("CharVar now represents a bracket ']' ");
-		
+
 		//Example 2 boolean:
-		
+
 		boolean cond=true;
 		System.out.println("cond = "+cond);
 		// cond= (byte) 0;
 		// Boolean cannot be assigned a byte value!
-		
+
 		// Example 3 integer:
 		int num=3;
 		double dob=2.0;
 		System.out.println("num= "+num);
 		System.out.println("dob= "+dob);
 		System.out.println("num*dob= "+num*dob); // Multiplying an integer by a double gives a double output
-		
+
 		// Investigating uninitialised variable:
-		
+
 		int j=1; // Declaring j as integer and setting initial value to 1
 		System.out.println("j= "+j);
 		int i;  /* Declaring i as integer WITHOUT setting initial value - this is useful for some memory conserving processes,
 				where value for i can be set later */
-		
+
 		// System.out.println("i= "+i); - Cannot print i as i has no value (just the variable type)
 		//j=i+1; -Operation cannot be performed: i not have been initialised
 		//System.out.println("j= "+j);
-		
+
 		// Investigating casting:
 
 		double double_var=4.99;  // Assigning a value of 4.99 to a double variable
--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -1,22 +1,53 @@
+/* This is Function/Methods assignment 
+ * 
+ * This script: 1) Calculates the magnitude of a vector with components x, y, z (as defined in the script)
+ * 				2) Calculates the dotProduct of two vectors with components a1, a2, a3 and b1, b2, b3
+ *				3) Calculates the angle of two vectors with components a1, a2, a3 and b1, b2, b3
+ * */
+
 package module1;
 
 public class VectorMethods {
-	
-	public (double) magnitude()
-	double x=1;
-	double y=2;
-	double z=3;
-	
 
-	
+	public VectorMethods(){}						// Creating an empty constructor 
+
+	public double magnitude (double x, double y, double z){  // Defining function magnitude with double as return type and inputs as x, y and z
+		return Math.sqrt(x*x+y*y+z*z);		// Defining the return of the function magnitude in terms of x, y an z 								
+	}
+
+	public double dotProduct (double a1, double a2, double a3, double b1, double b2, double b3){
+		return a1*b1+a2*b2+a3*b3;
+
+		public double angle ((double a1, double a2, double a3, double b1, double b2, double b3){
+
+		}
+	}
+
 	public static void main(String[] args) {
-		VectorMethods vm = new VectorMethods(); // Creating Vector Methods object
-		
-		double mag = vm.magnitude(x,y,z); // Creating a double variable magnitude which calls the function and returns the magnitude
-										// of a vector with components as variables x, y and z
-		
-		
-		
+		VectorMethods vm = new VectorMethods(); // Creating vm object of VectorMethods class 
+		double x=4; 
+		double y=7; 
+		double z=3;
+
+		double a1=6; 
+		double a2=5; 
+		double a3=1;
+		double b1=2; 
+		double b2=4; 
+		double b3=3;
+
+
+		double scalarProduct=vm.dotProduct(a1,a2,a3,b1,b2,b3);
+		System.out.println("Scalar product of two vectors= "+scalarProduct+ " (With components: a1= "+a1+ " a2= "+a2+ " a3= "+a3+ " and " 
+				+ "b1= " +b1+ " b2= "+b2+ " b3= "+b3+ ")");
+
+		double mag = vm.magnitude(x,y,z); // Creating a double variable magnitude which calls the function "magnitude" and returns the magnitude
+		// of a vector with components as variables x, y and z
+		System.out.println("Vector Magnitude= "+mag+ " (With components: x= "+x+ " y= "+y+ " and z= "+z+")");
+
+		double theta=vm.angle(a1,a2,a3,b1,b2,b3);
+		System.out.println("The angle between two vectors= "+theta+ " (With components: x= "+x+ " y= "+y+ " and z= "+z+")");
+
 	}
 
 }
