e38695d4ee1e157bec2e2523db984ecbd21e1fb0
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2014-10-13 16:35:57 +0100

    Testing Git (UCL > HOME)

--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -9,16 +9,16 @@ public class VectorMethods {
 
 	public VectorMethods(){}						// Creating an empty constructor 
 
-	public double magnitude (double x, double y, double z){  // Defining function magnitude with double as return type and inputs as x, y and z
-		return Math.sqrt(x*x+y*y+z*z);		// Defining the return of the function magnitude in terms of x, y an z 								
+	public double magnitude (double x1, double y1, double z1){  // Defining function magnitude with double as return type and inputs as x, y and z
+		return Math.sqrt(dotProduct(x1,y1,z1,x1,y1,z1));		// Defining the return of the function magnitude in terms of x, y an z 								
 	}
 
-	public double dotProduct (double x1, double x2, double x3, double y1, double y2, double y3){
-		return x1*y1+x2*y2+x3*y3;
+	public double dotProduct (double x1, double y1, double z1, double x2, double y2, double z2){
+		return x1*x2+y1*y2+z1*z2;
 	}
-	public double angle (double x1, double x2, double x3, double y1, double y2, double y3){			//Creating angle function which uses the outputs of functions 
+	public double angle (double x1, double y1, double z1, double x2, double y2, double z2){			//Creating angle function which uses the outputs of functions 
 																									//dotProduct and magnitude
-		return Math.toDegrees(Math.acos((dotProduct(x1,x2,x3,y1,y2,y3)/(magnitude(x1,x2,x3)*magnitude(y1,y2,y3)))));
+		return Math.toDegrees(Math.acos((dotProduct(x1,y1,z1,x2,y2,z2)/(magnitude(x1,y1,z1)*magnitude(x2,y2,z2)))));
 	}
 
 
---
+++ PHAS3459/src/module2/ThreeVector.java
@@ -0,0 +1,15 @@
+package module2;
+
+/*This class stores, and retrieve (as doubles) the x, y, z components of a 3-component vector. 
+*/
+
+public class ThreeVector {
+
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
