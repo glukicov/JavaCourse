e522b4164cc092d564f6fbe52be5babbd21834ef
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2014-11-12 15:56:15 +0000

    done

--- PHAS3459/src/exam1/Exoplanets.java
+++ PHAS3459/src/exam1/Exoplanets.java
@@ -7,9 +7,10 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
-import example_1.Particle;
 
 public class Exoplanets {
 
@@ -24,21 +25,18 @@ public class Exoplanets {
 
 	// The data storage class for ArrayList is PlanetData.java
 
+
+
 	public static void main(String[] args) {
 
 		//Exoplanet Data url (name, year, method [of discovery], mass [units of Jup. mass], separation [from parents strar /a.u.], 
 		// distance [/parsec] ): 
-		String dataURL=("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/exoplanets.txt");
+		String dataURL="http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/exoplanets.txt";
 
 		//Initialising ArrayList
 		ArrayList<PlanetData> data = new ArrayList<PlanetData>();
 
-		//Initialising variables:
-		//double minMass=Double.MAX_VALUE;
-		//double maxMass=Double.MIN_VALUE;
-		//int minID=0;
-		//int maxID=0;
-
+	
 		try{
 
 			//storing the planet data in the ArrayList
@@ -48,20 +46,13 @@ public class Exoplanets {
 			//Collections.sort(samples);	
 
 
-			//This method works for sorting out array list for doubles (mass) 
-			/*	Collections.sort(samples, new Comparator<Samples>() {
-			    @Override
-			    public int compare(Samples c1, Samples c2) {
-			        return Double.compare(c1.getMass(), c2.getMass());
-			    }
-			});*/
-
-
-
-
-
-
-
+			//This comparator method is used for sorting out array list for distance in ascending order to get the closest planet to Earth
+			Collections.sort(data, new Comparator<PlanetData>() {
+				@Override
+				public int compare(PlanetData c1, PlanetData c2) {
+					return Double.compare(c1.getDistance(), c2.getDistance());
+				}
+			});
 
 		}
 		catch (IOException e){
@@ -70,88 +61,15 @@ public class Exoplanets {
 
 		finally{
 
-			//ADD SOMETHING 
+			System.out.println("Tasks to analyse Exoplanets Data have been succesfully executed!");
 
 		}
-		/*System.out.println("Samples: "+samples.toString()+"\n");
-	System.out.println("Locations: "+locations+"\n");
+		
 
+	System.out.println("The planet closest to Earth is= "+data.get(0).getName()+ ", mass= "+data.get(0).getMass()+" year discovered= "+data.get(0).getYear());
+	
 
-
-	System.out.println("Samples after sorting: "+samples.toString()+"\n");
-	//System.out.println("Samples after sorting: "+samples+"\n");
-
-	System.out.println("The length of array"+ samples.size());		
-	System.out.println("The lightest element:" +samples.get(0).getMass());	
-	Object lightest=samples.get(0);
-//	System.out.println("The lightest mass:"+lightest.);
-
-	int maxID=samples.get(528).getCode();
-	int minID=samples.get(0).getCode();*/
-
-		// /g, location="+locations.get(maxID)+"\n"
-		//  /g, location="+locations.get(minID)+"\n"
-
-
-		//Implementing search though locations list array to find a specific location
-
-
-		/*
-	 int x = maxID;
-	 System.out.println("x"+x);
-	 int index2=locations.indexOf(x);
-	 System.out.println("index 2=" +index2);
-	// int index=Collections.binarySearch(locations, x);
-
-	   int liVal = -1;
-	   for(int i=0; i<locations.size();i++)
-	       if(x < locations.get(i).getCode()) // if we met a value > x
-	       {
-	          if(liVal==-1) // if we could not find any largest value smaller than x
-	              liVal = locations.get(i).getCode(); // return the value > x
-	          break;
-	       }
-	       else if(x > locations.get(i).getCode()) // find the largest value smaller than x, 
-	       {
-	           if(liVal < locations.get(i).getCode())
-	               liVal = locations.get(i).getCode();
-	       }
-
-	System.out.println(liVal);*/
-
-
-
-
-
-
-
-
-
-
-
-		/*System.out.println("Largest mass of a sample: ID= "+samples.get(528).getCode()+ ", mass= "+samples.get(528).getMass());
-	System.out.println("Smallest mass of a sample: ID= "+samples.get(0).getCode()+ ", mass= "+samples.get(0).getMass());
-
-		 */
-
-
-
-
-
-
-
-
-
-		//	int index = locations.indexOf(28);
-		//	System.out.println("index="+index);
-
-		//TO-DO later: Combining Hashmas into 1 with 3 objects is it possible
-		//or use other collection like:
-		/*		HashMap<Integer, Double, String>combined = new HashMap();
-		combined.putAll(samples);
-		combined.putAll(locations)
-		 */
-
+		
 	}
 
 
@@ -165,43 +83,43 @@ public class Exoplanets {
 		BufferedReader br = new BufferedReader(isr);
 		//Using a scanner to conveniently look through the data
 		Scanner s=new Scanner(br);
+		s.useDelimiter(",");
 
-		while (s.hasNextLine()){
-			//Definition of data handling:
-			// Ignoring first line
-			String firstLine=s.nextLine();
-			// New scanner to look through data
-			Scanner scann=new Scanner(firstLine);
-			
-			//Looping over each line:
-			for (int i=0; i<events; i++){
-				String dataLine=scann.nextLine();
-				Scanner datascann=new Scanner(dataLine);
+		s.nextLine();
 
-				String name = datascann.next("[a-zA-Z]*");
-				Integer year = Integer.parseInt(datascann.next());
-				String method = datascann.next("[a-zA-Z]*");
-				Double mass = Double.parseDouble(datascann.next());
-				Double separation = Double.parseDouble(datascann.next());
-				Double distance = Double.parseDouble(datascann.next());
-				
-				arrayList.add(new PlanetData(name, year, method, mass, separation, distance));
+		s.useDelimiter(",");
+
+		while(s.hasNextLine()) try{
+			double distance=0; //extending variable scope 
+
+			String name = s.next();
+			Integer year = s.nextInt();
+			String method =s.next();
+			Double mass = s.nextDouble();
+			Double separation = s.nextDouble();
+			// IOException to handle if no distance is provided:
+			try{
+				distance = s.nextDouble();
+				System.out.println("distance="+distance);
+			}
+			catch (Exception e)
+			{
+				break;	
 			}
 
+			arrayList.add(new PlanetData(name, year, method, mass, separation, distance));
+			s.close();
+
+		}	
+		finally{
 		}
-		/*while(s.hasNext()){
-			String location =s.next("[a-zA-Z]*") ;
-			arrayList.add(new PlanetData(location, 
-					s.nextInt()));
-		}
-		 */
 
 
-
-		//Closing scanner explicitly to flush all data
-		s.close();
+		//s.close();
 		return arrayList;
 	}
+
+
 
 }
 
--- PHAS3459/src/exam1/PlanetData.java
+++ PHAS3459/src/exam1/PlanetData.java
@@ -1,6 +1,7 @@
 package exam1;
 
 //This class defines our data type handling to be used in the array list 
+//Using comparable implementation to create a custom sort for the ListArray
 
 public class PlanetData {
 
@@ -17,16 +18,28 @@ public class PlanetData {
 		 this.name=name; this.year=year; this.method=method; this.mass=mass; this.separation=separation;this.distance=distance;
 	}
 	//Getter methods
-	public String getLocation() {
-		return this.location;
+	public String getName() {
+		return this.name;
 	}
-	public int getCode() {
-		return this.code;
+	public int getYear() {
+		return this.year;
+	}
+	public String getMethod() {
+		return this.method;
+	}
+	public double getMass() {
+		return this.mass;
+	}
+	public double getSeparation() {
+		return this.separation;
+	}
+	public double getDistance() {
+		return this.distance;
+	}
 	
-	}
 	//Method to output values as a string
 		public String toString() {
-			return "("+this.location+", "+this.code+")";
+			return "("+this.name+", "+this.year+", "+this.method+", "+this.mass+", "+this.separation+", "+this.distance+",)";
 		}
 
 }
